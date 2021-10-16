0d661810e6499edfaf3233c87ab2ceb95831d8c6
Hugo Mendoza <hmendoza@serempre.com>
2021-10-16 15:59:47 -0500

    avance epic react

---
+++ testing-react-apps/src/__tests__/exercise/01.js
@@ -0,0 +1,42 @@
+// simple test with ReactDOM
+// http://localhost:3000/counter
+
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import Counter from '../../components/counter'
+
+test('counter increments and decrements when the buttons are clicked', () => {
+  const div = document.createElement("div")
+  document.body.append(div)
+  ReactDOM.render(<Counter/>, div)
+
+  const [decrement, increment] = document.body.querySelectorAll("button")
+  const message = div.firstChild.querySelector("div")
+  
+  expect(message.textContent).toBe("Current count: 0")
+
+  const incrementClickEvent = new MouseEvent("click", {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+
+  increment.dispatchEvent(incrementClickEvent)
+  expect(message.textContent).toBe("Current count: 1")
+
+  const decrementClickEvent = new MouseEvent("click", {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+  
+  decrement.dispatchEvent(decrementClickEvent)
+  expect(message.textContent).toBe("Current count: 0")
+  
+  div.remove()
+
+  // console.log(document.body.innerHTML)
+  // console.log(message.textContent)
+})
+
+/* eslint no-unused-vars:0 */
